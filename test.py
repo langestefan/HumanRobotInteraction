f7a661bb6270f440fafa0ab1ec0d69417100ace1
Stefan de Lange <langestefan@MSI.localdomain>
2022-05-03 09:33:54 +0200

    Add text encoding fix for vscode terminal

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import nao_nocv_2_1 as nao  # with this line you import all the functions of the naolibrary and are able to call them with “nao”
 nao.InitProxy("127.0.0.1")  # with this line you initialize all the proxies, and indicate the IP-adres. In this case this is 127.0.0.1, because this is your home ip-adress.
 nao.InitPose()              # this line makes the robot stand up
