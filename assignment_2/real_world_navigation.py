c314e30fa25616d0c9e66699d8faa961368fb830
Stefan <langestefan@msn.com>
2022-06-03 16:43:15 +0200

    Add landmark detection to scanning sequence
    Signed-off-by: Stefan <langestefan@msn.com>

--- assignment_2/real_world_navigation.py
+++ assignment_2/real_world_navigation.py
@@ -1,11 +1,11 @@
+from turtle import distance
 import nao_nocv_2_1 as nao
 from time import sleep
 import numpy as np
 
-marvin = {'ip_address' : '192.168.0.115', 'colour' :'orange'}
-bender = {'ip_address' : '192.168.0.112', 'colour' :'red'}
+from landmark_detection import compute_landmark_location
 
-# eve = {'ip_address' : '192.168.0.119', 'colour' :'white'}
+from definitions import marvin
 
 
 def detectLandmarkLoop(interval=0.1, max_duration=5):
@@ -21,31 +21,46 @@ def detectLandmarkLoop(interval=0.1, max_duration=5):
         cur_duration += interval
 
 
-def StaticScan(yaw_val_deg=(0, 0)):
+def StaticScan(yaw_val_deg=(0, 0), pitch_val_deg=-20):
     # yaw val in radians from degrees
     yaw_val_rad_left = np.radians(yaw_val_deg[0])
     yaw_val_rad_right = np.radians(yaw_val_deg[1])
 
     # create a list of yaw values
-    n_angles = 100
+    n_angles = 20
     t_offset = 0
-    degrees_per_sec = 2
-    rads_per_sec = np.radians(degrees_per_sec)
+    degrees_per_sec = 50
+    # rads_per_sec = np.radians(degrees_per_sec)
 
+    # pitch val in rads
+    pitch_val_rad = np.radians(pitch_val_deg)
+
+    # time it takes to move from left to right should depend on total distance we move
     max_time_sec = (yaw_val_deg[1] - yaw_val_deg[0]) / degrees_per_sec
+    print("max_time_sec: ", max_time_sec)
+
     yaw_val_rad = np.linspace(yaw_val_rad_left, yaw_val_rad_right, num=n_angles).tolist()
-    yaw_timelist = np.linspace(t_offset, max_time_sec+t_offset, num=n_angles).tolist()
+    yaw_timelist = np.linspace(t_offset, max_time_sec+t_offset, num=n_angles+1).tolist()
 
     # scan the environment with Nao's head, not moving the robot
-    for idx, angle_rad in enumerate(yaw_val_rad):
-        print(f"{idx} : {angle_rad}")
+    for idx, yaw_angle_rad in enumerate(yaw_val_rad):
+        print("{0} : {1}".format(idx, yaw_angle_rad))
         sleep(yaw_timelist[idx+1] - yaw_timelist[idx])
-        nao.MoveHead(angle_rad, pitch_val=0, post=False)
+        nao.MoveHead(yaw_angle_rad, pitch_val=pitch_val_rad, post=False)
 
-    # scan the environment with Nao's head, not moving the robot
-    # nao.MoveHead(yaw_val=yaw_val_rad_left, pitch_val=0)
-    # sleep(5)
-    # nao.MoveHead(yaw_val=yaw_val_rad_right, pitch_val=0)
+        # look for landmark
+        detected, timestamp, markerInfo = nao.DetectLandMark()
+
+        if detected:
+            print("timestamp: ", timestamp)
+            # print("markerInfo: ", markerInfo)
+            distance, angle_rad, angle_deg = compute_landmark_location(markerInfo)   
+            print("Target found at distance: {0} angle: {1}".format(distance, angle_deg))     
+
+        else:
+            print("Didn't detect target")
+
+    print("Done scanning")
 
     # detectLandmarkLoop(max_duration=10)
 
@@ -66,9 +81,12 @@ def main():
     # SonarLeft, SonarRight = nao.ReadSonar()
     # print("SonarLeft, SonarRight: ", SonarLeft, SonarRight)
 
+    # init landmark
+    nao.InitLandMark(switch=True, period = 500)
+
     # scan the environment with Nao's head, not moving the robot
     # positive = left, negative = right?
-    StaticScan(yaw_val_deg=(-60, 60))
+    StaticScan(yaw_val_deg=(-90, 90))
     sleep(10)
         
     # nao crouch
